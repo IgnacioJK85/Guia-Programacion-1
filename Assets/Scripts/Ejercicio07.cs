a6f400c2b653c29996be1178067fd711b6968bf4
Ignacio Kirchuk <ignajk369@gmail.com>
2022-04-09 13:50:56 -0300

    Ejercicio07Terminado

---
+++ Assets/Scripts/Ejercicio07.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio07 : MonoBehaviour
+{
+    public static int num1, num2, num3;
+    public int[] nums = new int[] { num1, num2, num3 };
+    // Start is called before the first frame update
+    void Start()
+    {
+        int largest = nums[0];
+        for (int big = 1; big < nums.Length; big++)
+        {
+            if (largest < nums[big])
+            {
+                largest = nums[big];
+            }
+        }
+
+        Debug.Log(largest);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
